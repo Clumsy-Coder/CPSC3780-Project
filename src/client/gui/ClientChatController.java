731fde91da1d75fc1c74bc591231bf1fedcfeaeb
Clumsy-Coder <ubari94@gmail.com>
2016-11-07 12:13:51 -0700

    Add code to display JFXDialog when the Client Chat starts. Asks for username, firstname, lastname and serverIP. This is hardcoded code. NOT using dialog.fxml file to generate the JFXDialog.
    using the fxml file is desirable.
    
    Future functionality would be
    to read in the information and check the values
    are valid. Making sure all the fields are filled.
    Check the username and make sure it's unique
    and the Client can connect to the server before proceeding.

--- src/client/gui/ClientChatController.java
+++ src/client/gui/ClientChatController.java
@@ -1,35 +1,73 @@
 package client.gui;
 
-import com.jfoenix.controls.JFXListView;
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleListProperty;
+import com.jfoenix.controls.*;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
 import java.util.ResourceBundle;
 
 /**
- * Created by Umar on 2016-11-03.
+ * @author Umar
+ *         Class used to respond to events from clientGUI.fxml
  */
 public class ClientChatController implements Initializable
 {
-	private ArrayList<Label>   users = new ArrayList<Label>();
+	private ArrayList<Label> users = new ArrayList<Label>();
 	@FXML
 	private JFXListView<Label> userList;
 	@FXML
 	private JFXListView<Label> conversationList;
 
+	@FXML
+	private StackPane stackPane;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
-		for(int i = 0; i < 20; i++)
+
+		//open dialog that contains text field for username, firstname,
+		//lastname and server IP address
+
+		JFXDialogLayout content = new JFXDialogLayout();
+		content.setHeading(new Text("Setup"));
+		VBox vbox = new VBox(10);
+		vbox.getChildren()
+				.addAll(new JFXTextField("Username"), new JFXTextField("Firstname"), new JFXTextField("Lastname"),
+				        new JFXTextField("Server IP"));
+		content.setBody(vbox);
+		JFXDialog dialog = new JFXDialog(stackPane, content, JFXDialog.DialogTransition.CENTER);
+		JFXButton button = new JFXButton("Connect");
+		button.setOnAction(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				dialog.close();
+			}
+		});
+
+		content.setActions(button);
+		dialog.show();
+
+//			JFXDialogLayout dialogBoxLayout = FXMLLoader.load(getClass().getResource("resource/dialog.fxml"));
+//			JFXDialog       dialogBox       = new JFXDialog(new StackPane(), dialogBoxLayout,/*
+//			                                                JFXDialog.DialogTransition.CENTER);*/
+//			JFXDialog dialogBox = new JFXDialog();
+//
+//			dialogBox.setContent(dialogBoxLayout);
+//			dialogBox.setTransitionType(JFXDialog.DialogTransition.CENTER);
+//			dialogBox.show();
+
+		for (int i = 0; i < 20; i++)
 		{
 			users.add(new Label("User " + i));
 		}
@@ -37,7 +75,6 @@ public class ClientChatController implements Initializable
 //		userList.itemsProperty().bind(listProerty);
 		userList.setItems(FXCollections.observableArrayList(users));
 //		conversationList.setItems(FXCollections.observableArrayList(users));
-
 
 
 	}
