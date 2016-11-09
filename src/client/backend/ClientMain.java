49cee5e6a0b24b7599d3864b44f53a288a0b904f
Clumsy-Coder <ubari94@gmail.com>
2016-11-08 20:00:58 -0700

    Make a Client main class to connect to the server and send messages.
    Currently, able to send mesages. Server recieves the message
    Also send GET requests. Server recieves the message and responds.
    Server responds with a message. Client prints out GET request was responded to.

---
+++ src/client/backend/ClientMain.java
@@ -0,0 +1,48 @@
+package client.backend;
+
+import utilities.User;
+
+import java.util.Scanner;
+
+/**
+ * Created by Umar on 2016-11-08.
+ */
+public class ClientMain
+{
+	public static void main(String[] args)
+	{
+		int    port     = 5555;
+		String serverIP = "localhost";
+		String username = "Anonymous";
+
+		User   user   = new User(username);
+		Client client = new Client(serverIP, port, user);
+
+		if (!client.connect())
+		{
+			return;
+		}
+
+		Scanner scan      = new Scanner(System.in);
+		boolean keepGoing = true;
+		while (keepGoing)
+		{
+			System.out.print(username + "> ");
+			String line = scan.nextLine();
+			//if logout was typed
+			if (line.equalsIgnoreCase("LOGOUT"))
+			{
+				//send a message to the server that you are disconnecting.
+				keepGoing = false;
+				System.out.println("Logged off");
+
+			}
+			else
+			{
+				client.sendMessage(line, new User("Anonymous2"));
+			}
+		}
+		client.disconnect();
+
+	}
+}
