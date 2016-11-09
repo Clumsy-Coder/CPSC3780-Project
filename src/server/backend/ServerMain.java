536c72c242b2f8fb65add21bbd89564a926aa829
Clumsy-Coder <ubari94@gmail.com>
2016-11-08 17:29:33 -0700

    Make a main class to start the server

---
+++ src/server/backend/ServerMain.java
@@ -0,0 +1,19 @@
+package server.backend;
+
+import java.io.IOException;
+
+/**
+ * Created by Umar on 2016-11-08.
+ */
+public class ServerMain
+{
+	public static void main(String [] args) throws
+	                                        IOException,
+	                                        ClassNotFoundException
+	{
+		Server server = new Server();
+		server.startServer();
+		System.out.println("random text in ServerMain.java");
+		server.stopServer();
+	}
+}
