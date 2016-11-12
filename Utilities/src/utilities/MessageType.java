9735501f36ed8ae0092f28ce6d67ce1589775fae
Clumsy-Coder <ubari94@gmail.com>
2016-11-11 20:17:51 -0700

    Add another enum NEW_SERVER in MessageType class
    This is to be implemented later.
    It's for introducing a new server in the network. When it's connecting to a exsting server.

--- Utilities/src/utilities/MessageType.java
+++ Utilities/src/utilities/MessageType.java
@@ -4,12 +4,12 @@ package utilities;
  * Enum used for keeping track what type of message is being sent.<br>
  * SEND: for sending a message<br>
  * GET: for retrieving a message<br>
- * ACK: for acknowledging a message has been recieved<br>
+ * ACK: for acknowledging a message has been received<br>
  * USERS: for sending information about what user is connected to the server.
  * DISCONNECT: for notifying the server the client will be disconnecting
  * CONNECT: for notifying the server the client will be connecting.
  */
 public enum MessageType
 {
-    SEND, GET, ACK, USERS, DISCONNECT, CONNECT
+    SEND, GET, ACK, USERS, DISCONNECT, CONNECT, NEW_SERVER
 }
